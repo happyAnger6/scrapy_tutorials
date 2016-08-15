be84fa8fd9648c9f1a0b8121d931f3bcc88064a6
happyAnger6 <happyAnger6@163.com>
2016-08-15 06:21:27 -0700

    修改爬虫适应奥运页面改动

--- tutorials/spiders/ghostsina.py
+++ tutorials/spiders/ghostsina.py
@@ -5,6 +5,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from bs4 import BeautifulSoup
+from ghost import Ghost
 
 from scrapy.spiders import CrawlSpider,Rule
 from scrapy.linkextractors import LinkExtractor
@@ -12,19 +13,19 @@ from scrapy.http.response.html import HtmlResponse
 
 from ..items import NewsItem
 
-class PhantomjsSpider(CrawlSpider):
+class GhostSinaSpider(CrawlSpider):
     def __init__(self,*a,**kw):
-        super(PhantomjsSpider,self).__init__(*a,**kw)
+        super(GhostSinaSpider,self).__init__(*a,**kw)
 
 
-    name = "sina_oly_phantomjs"
+    name = "sina_oly_ghost"
     #allowed_domains = ["www.51job.com"]
     start_urls = (
         'http://2016.sina.com.cn/',
     )
 
     rules = (
-        Rule(LinkExtractor(allow=('2016.sina.com.cn/china/[0-9\-]*/doc-if[a-z0-9]*.shtml',)),process_request='phantomjs_process',callback='parse_one_news',follow=True),
+        Rule(LinkExtractor(allow=('2016.sina.com.cn/china/[0-9\-]*/doc-if[a-z0-9]*.shtml',)),process_request='ghost_process',callback='parse_one_news',follow=True),
         Rule(LinkExtractor(allow=('2016.sina.com.cn/brazil/[0-9\-]*/doc-if[a-z0-9]*.shtml',
                                   '2016.sina.com.cn/side/[0-9\-]*/doc-if[a-z0-9]*.shtml')),
                                     process_request='phantomjs_process',callback='parse_one_news',follow=True),
@@ -53,9 +54,10 @@ class PhantomjsSpider(CrawlSpider):
             for word in keywords:
                 keywords_lst.append(word.css("a::text").extract()[0])
             item['keywords'] = keywords_lst
-            comments = response.css("div[id='j_commentlist'] span[class='count'] a::text").extract()
-            item['involves'] = comments[0].replace(',','')
-            item['comments'] = comments[1].replace(',','')
+            print("ddd",response.css("div[id='j_commentlist'] span[class='count'] a::text").extract())
+            counts = response.css("p[class='post_box_count'] span[class='f_red']::text").extract()
+            item['involves'] = counts[1].replace(',','')
+            item['comments'] = counts[0].replace(',','')
             item['hot'] = float(item['involves'])*0.3 + float(item['comments'])*0.7
             '''
             filename = response.url.split("/")[-2] + '.html'
@@ -67,7 +69,7 @@ class PhantomjsSpider(CrawlSpider):
             return []
         return item
 
-    def phantomjs_process(self,request):
+    def ghost_process(self,request):
         def do_counts(str_counts):
             try:
                 counts = str_counts.replace(',','')
@@ -78,13 +80,13 @@ class PhantomjsSpider(CrawlSpider):
             if item and isinstance(item,list):
                 return item[0]
             return item
+        url = request.url
         try:
-            url = request.url
-            driver = webdriver.PhantomJS(executable_path="/usr/bin/phantomjs")
-            driver.get(request.url)
-            body = driver.page_source
-            response = HtmlResponse(url,body=body.encode('UTF-8'),request=request)
+            ghost = Ghost()
+            with ghost.start() as session:
+                page,extra_resouce = session.open(url)
+            response = HtmlResponse(url,body=page.content.encode('UTF-8'),request=request)
         except Exception as e:
-            self.logger.error("phantomjs error:",e,url)
+            self.logger.error("ghost.py error:",e,url)
             return []
         return self.parse_one_news(response)
